5b48080703f940e8125dc63bc00af35fb338a6e0
Amacito <amarachi5695@gmmail.com>
2022-11-14 02:20:47 +0100

    completed

--- currency_converter/allcurrency_app.js
+++ currency_converter/allcurrency_app.js
@@ -8,14 +8,14 @@ console.log("Application for all currency converter app");
 
 alert("Hi, welcome to your all currency converter");
 
-var currencyType1 = prompt("what currency do you want to convert? e.g USD, Naira, GBP, EUR, Yen, etc.");
-var currencyType2 = prompt("what currency do you want to convert to? e.g USD, Naira, GBP, EUR, Yen, etc.");
+var currencyType1 = prompt("what currency do you want to convert? e.g USD, NGN, GBP, EUR, GHC, JPY etc.");
+var currencyType2 = prompt("what currency do you want to exchange? e.g USD, NGN, GBP, EUR, GHC, JPY etc.");
 
 if(currencyType1 == "USD") {
     var currencyamount = prompt("How much [USD] do you want to convert?");
-    if(currencyType2 == "Naira") {
-        alert(1 + "USD" + " " + "=" + " " + 980 + " " + "Naira");
-        alert("Amount converted =" + " " + currencyamount * 980 + " " + "Naira");
+    if(currencyType2 == "NGN") {
+        alert("Conversion rate:" + " " + 1 + " " + "$" + "=" + " " + 700 + " " + "₦");
+        alert("Amount converted [USD] =" + " " + currencyamount * 700 + " " + "₦");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -23,8 +23,8 @@ if(currencyType1 == "USD") {
         alert("Thanks and Goodbye!!!");
     }
     }else if (currencyType2 == "GBP") {
-        alert(1 + "USD" + " " + "=" + " " + 800 + " " + "GBP");
-        alert("Amount converted =" + " " + currencyamount * 800 + " " + "GBP");
+        alert("Conversion rate:" + " " + 1 +" " +  "$" + " " + "=" + " " + 0.87524 + " " + "£");
+        alert("Amount converted [USD] =" + " " + currencyamount * 0.87524 + " " + "£");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -32,26 +32,26 @@ if(currencyType1 == "USD") {
         alert("Thanks and Goodbye!!!");
     }
     }else if (currencyType2 == "EUR") {
-        alert(1 + "USD" + " " + "=" + " " + 750 + " " + "EUR");
-        alert("Amount converted =" + " " + currencyamount * 750 + " " + "EUR");
+        alert("Conversion rate:" + " " + 1 + " " + "$" + " " + "=" + " " + 0.99676 + " " + "€");
+        alert("Amount converted [USD] =" + " " + currencyamount * 0.99676 + " " + "€");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
     } else if (transaction_continues == "no"){
         alert("Thanks and Goodbye!!!");
     }
-    }else if (currencyType2 == "Cedis") {
-        alert(1 + "USD" + " " + "=" + " " + 600 + " " + "Cedis");
-        alert("Amount converted =" + " " + currencyamount * 600 + " " + "Cedis");
+    }else if (currencyType2 == "GHC") {
+        alert("Conversion rate:" + " " + 1 + " " + "$" + " " + "=" + " " + 14.09699 + " " + "GH¢");
+        alert("Amount converted [USD] =" + " " + currencyamount * 14.09699 + " " + "GH¢");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
     } else if (transaction_continues == "no"){
         alert("Thanks and Goodbye!!!");
     }
-    }else if (currencyType2 == "Yen") {
-        alert(1 + "USD" + " " + "=" + " " + 300 + " " + "Yen");
-        alert("Amount converted =" + " " + currencyamount * 300 + " " + "Yen");
+    }else if (currencyType2 == "JPY") {
+        alert("Conversion rate:" + " " + 1 + " " + "$" + " " + "=" + " " + 146.31412 + " " + "¥");
+        alert("Amount converted [USD] =" + " " + currencyamount * 146.31412 + " " + "¥");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -59,11 +59,12 @@ if(currencyType1 == "USD") {
         alert("Thanks and Goodbye!!!");
     }
     }
-}else if(currencyType1 == "Naira") {
-    var currencyamount = prompt("How much [Naira] do you want to convert?");
+
+}else if(currencyType1 == "NGN") {
+    var currencyamount = prompt("How much [NGN] do you want to convert?");
     if(currencyType2 == "USD") {
-        alert(1 + "Naira" + " " + "=" + " " + 0.00102 + " " + "USD");
-        alert("Amount converted =" + " " + currencyamount * 0.00102 + " " + "USD");
+        alert("Conversion rate:" + " " + 1 + " " + "₦" + "=" + " " + 0.00142857 + " " + "$");
+        alert("Amount converted [NGN] =" + " " + currencyamount * 0.00142857 + " " + "$");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -71,8 +72,8 @@ if(currencyType1 == "USD") {
         alert("Thanks and Goodbye!!!");
     }
     }else if (currencyType2 == "GBP") {
-        alert(1 + "Naira" + " " + "=" + " " + 0.00125 + " " + "GBP");
-        alert("Amount converted =" + " " + currencyamount * 0.00125 + " " + "GBP");
+        alert("Conversion rate:" + " " + 1 + " " + "₦" + "=" + " " + 0.00197 + " " + "£");
+        alert("Amount converted [NGN] =" + " " + currencyamount * 0.00197 + " " + "£");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -80,26 +81,26 @@ if(currencyType1 == "USD") {
         alert("Thanks and Goodbye!!!");
     }
     }else if (currencyType2 == "EUR") {
-        alert(1 + "Naira" + " " + "=" + " " + 0.001333 + " " + "EUR");
-        alert("Amount converted =" + " " + currencyamount * 0.001333 + " " + "EUR");
+        alert("Conversion rate:" + " " + 1 + " " + "₦" + "=" + " " + 0.00225 + " " + "€");
+        alert("Amount converted [NGN] =" + " " + currencyamount * 0.00225 + " " + "€");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
     } else if (transaction_continues == "no"){
         alert("Thanks and Goodbye!!!");
     }
-    }else if (currencyType2 == "Cedis") {
-        alert(1 + "Naira" + " " + "=" + " " + 0.001666 + " " + "Cedis");
-        alert("Amount converted =" + " " + currencyamount * 0.001666 + " " + "Cedis");
+    }else if (currencyType2 == "GHC") {
+        alert("Conversion rate:" + " " + 1 + " " + "₦" + "=" + " " + 0.03176 + " " + "GH¢");
+        alert("Amount converted [NGN] =" + " " + currencyamount * 0.03176 + " " + "GH¢");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
     } else if (transaction_continues == "no"){
         alert("Thanks and Goodbye!!!");
     }
-    }else if (currencyType2 == "Yen") {
-        alert(1 + "Naira" + " " + "=" + " " + 0.00333 + " " + "Yen");
-        alert("Amount converted =" + " " + currencyamount * 0.00333 + " " + "Yen");
+    }else if (currencyType2 == "JPY") {
+        alert("Conversion rate:" + " " + 1 + " " + "₦" + "=" + " " + 0.32968 + " " + "¥");
+        alert("Amount converted [NGN] =" + " " + currencyamount * 0.32968 + " " + "¥");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -107,20 +108,21 @@ if(currencyType1 == "USD") {
         alert("Thanks and Goodbye!!!");
     }
     }
+
 }else if(currencyType1 == "GBP") {
     var currencyamount = prompt("How much [GBP] do you want to convert?");
     if(currencyType2 == "USD") {
-        alert(1 + "GBP" + " " + "=" + " " + 0.00125 + " " + "USD");
-        alert("Amount converted =" + " " + currencyamount * 0.00125 + " " + "USD");
+        alert("Conversion rate:" + " " + 1 + " " + "£" + "=" + " " + 1.14255 + " " + "$");
+        alert("Amount converted [GBP] =" + " " + currencyamount * 1.14255 + " " + "$");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
     } else if (transaction_continues == "no"){
         alert("Thanks and Goodbye!!!");
     }
-    }else if (currencyType2 == "Naira") {
-        alert(1 + "GBP" + " " + "=" + " " + 0.00125 + " " + "Naira");
-        alert("Amount converted =" + " " + currencyamount * 0.00125 + " " + "Naira");
+    }else if (currencyType2 == "NGN") {
+        alert("Conversion rate:" + " " + 1 + " " + "£" + "=" + " " + 507.06370 + " " + "₦");
+        alert("Amount converted [GBP] =" + " " + currencyamount * 507.06370 + " " + "₦");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -128,26 +130,173 @@ if(currencyType1 == "USD") {
         alert("Thanks and Goodbye!!!");
     }
     }else if (currencyType2 == "EUR") {
-        alert(1 + "GBP" + " " + "=" + " " + 0.0000266 + " " + "EUR");
-        alert("Amount converted =" + " " + currencyamount * 0.0000266 + " " + "EUR");
+        alert("Conversion rate:" + " " + 1 + " " + "£" + "=" + " " + 1.13885 + " " + "€");
+        alert("Amount converted [GBP] =" + " " + currencyamount * 1.13885 + " " + "€");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
     } else if (transaction_continues == "no"){
         alert("Thanks and Goodbye!!!");
     }
-    }else if (currencyType2 == "Cedis") {
-        alert(1 + "GBP" + " " + "=" + " " + 0.000066 + " " + "Cedis");
-        alert("Amount converted =" + " " + currencyamount * 0.000066 + " " + "Cedis");
+    }else if (currencyType2 == "GHC") {
+        alert("Conversion rate:" + " " + 1 + " " + "£" + "=" + " " + 16.10652 + " " + "GH¢");
+        alert("Amount converted [GBP] =" + " " + currencyamount * 0.03176 + " " + "GH¢");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
     } else if (transaction_continues == "no"){
         alert("Thanks and Goodbye!!!");
     }
-    }else if (currencyType2 == "Yen") {
-        alert(1 + "GBP" + " " + "=" + " " + 0.000033 + " " + "Yen");
-        alert("Amount converted =" + " " + currencyamount * 0.000033 + " " + "Yen");
+    }else if (currencyType2 == "JPY") {
+        alert("Conversion rate:" + " " + 1 + " " + "£" + "=" + " " + 167.17121 + " " + "¥");
+        alert("Amount converted [GBP] =" + " " + currencyamount * 167.17121 + " " + "¥");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }
+
+}else if(currencyType1 == "EUR") {
+    var currencyamount = prompt("How much [EUR] do you want to convert?");
+    if(currencyType2 == "USD") {
+        alert("Conversion rate:" + " " + 1 + " " + "€" + "=" + " " + 1.00325 + " " + "$");
+        alert("Amount converted [EUR] =" + " " + currencyamount * 1.00325 + " " + "$");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "NGN") {
+        alert("Conversion rate:" + " " + 1 + " " + "€" + "=" + " " + 445.24240 + " " + "₦");
+        alert("Amount converted [EUR] =" + " " + currencyamount * 445.24240 + " " + "₦");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "GBP") {
+        alert("Conversion rate:" + " " + 1 + " " + "€" + "=" + " " + 0.87808 + " " + "£");
+        alert("Amount converted [EUR] =" + " " + currencyamount * 0.87808 + " " + "£");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "GHC") {
+        alert("Conversion rate:" + " " + 1 + " " + "€" + "=" + " " + 14.14281 + " " + "GH¢");
+        alert("Amount converted [EUR] =" + " " + currencyamount * 14.14281 + " " + "GH¢");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "JPY") {
+        alert("Conversion rate:" + " " + 1 + " " + "€" + "=" + " " + 146.78966 + " " + "¥");
+        alert("Amount converted [EUR] =" + " " + currencyamount * 146.78966 + " " + "¥");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }
+
+}else if(currencyType1 == "GHC") {
+    var currencyamount = prompt("How much [GHC] do you want to convert?");
+    if(currencyType2 == "USD") {
+        alert("Conversion rate:" + " " + 1 + " " + "GH¢" + "=" + " " + 0.07094 + " " + "$");
+        alert("Amount converted [GHC] =" + " " + currencyamount * 0.07094 + " " + "$");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "NGN") {
+        alert("Conversion rate:" + " " + 1 + " " + "GH¢" + "=" + " " + 31.48190 + " " + "₦");
+        alert("Amount converted [GHC] =" + " " + currencyamount * 31.48190 + " " + "₦");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "GBP") {
+        alert("Conversion rate:" + " " + 1 + " " + "GH¢" + "=" + " " + 0.06209 + " " + "£");
+        alert("Amount converted [GHC] =" + " " + currencyamount * 0.06209 + " " + "£");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "EUR") {
+        alert("Conversion rate:" + " " + 1 + " " + "GH¢" + "=" + " " + 0.07071 + " " + "€");
+        alert("Amount converted [GHC] =" + " " + currencyamount * 0.07071 + " " + "€");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "JPY") {
+        alert("Conversion rate:" + " " + 1 + " " + "GH¢" + "=" + " " + 10.37910 + " " + "¥");
+        alert("Amount converted [GHC] =" + " " + currencyamount * 10.37910 + " " + "¥");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }
+
+}else if(currencyType1 == "JPY") {
+    var currencyamount = prompt("How much [JPY] do you want to convert?");
+    if(currencyType2 == "USD") {
+        alert("Conversion rate:" + " " + 1 + " " + "¥" + "=" + " " + 0.00683 + " " + "$");
+        alert("Amount converted [JPY] =" + " " + currencyamount * 0.00683 + " " + "$");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "NGN") {
+        alert("Conversion rate:" + " " + 1 + " " + "¥" + "=" + " " + 3.03320 + " " + "₦");
+        alert("Amount converted [JPY] =" + " " + currencyamount * 3.03320 + " " + "₦");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "GBP") {
+        alert("Conversion rate:" + " " + 1 + " " + "¥" + "=" + " " + 0.00598 + " " + "£");
+        alert("Amount converted [JPY] =" + " " + currencyamount * 0.00598 + " " + "£");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "GHC") {
+        alert("Conversion rate:" + " " + 1 + " " + "¥" + "=" + " " + 0.09635 + " " + "GH¢");
+        alert("Amount converted [JPY] =" + " " + currencyamount * 0.09635 + " " + "GH¢");
+        var transaction_continues = prompt("Do you want to perform another transaction?");
+    if(transaction_continues == "yes"){
+        alert("Reload page and continue. Thanks!!!");
+    } else if (transaction_continues == "no"){
+        alert("Thanks and Goodbye!!!");
+    }
+    }else if (currencyType2 == "EUR") {
+        alert("Conversion rate:" + " " + 1 + " " + "¥" + "=" + " " + 0.00681 + " " + "€");
+        alert("Amount converted [JPY] =" + " " + currencyamount * 0.00681 + " " + "€");
         var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
--- currency_converter/naira_app.js
+++ currency_converter/naira_app.js
@@ -8,12 +8,12 @@ console.log("Application for naira currency converter app");
 
 alert("Hi, welcome to your naira currency converter");
 
-var currencyType = prompt("what currency do you want to convert? e.g USD, Naira, GBP, EUR, Yen, etc.");
+var currencyType = prompt("what currency do you want to convert to NGN? e.g USD, GBP, EUR, GHC, JPY, etc.");
 
 if(currencyType == "USD") {
-    alert(1 + "USD" + " " + "=" + " " + 980 + " " + "Naira");
-    var currencyamount = prompt("How much do you want to convert?");
-    alert("Amount converted =" + " " + currencyamount * 980 + " " + "Naira");
+    alert("Conversion rate:" + " " + 1 + " " + "$" + "=" + " " + 700 + " " + "₦");
+    var currencyamount = prompt("How much [USD] do you want to convert?");
+    alert("Amount converted =" + " " + currencyamount * 700 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -21,9 +21,9 @@ if(currencyType == "USD") {
         alert("Thanks and Goodbye!!!");
     }
 }else if (currencyType == "GBP") {
-    alert(1 + "GBP" + " " + "=" + " " + 800 + " " + "Naira");
-    var currencyamount = prompt("How much do you want to convert?");
-    alert("Amount converted =" + " " + currencyamount * 800 + " " + "Naira");
+    alert("Conversion rate:" + " " + 1 +" " +  "£" + " " + "=" + " " + 507.06370 + " " + "₦");
+    var currencyamount = prompt("How much [GBP] do you want to convert?");
+    alert("Amount converted =" + " " + currencyamount * 507.06370 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -31,29 +31,29 @@ if(currencyType == "USD") {
         alert("Thanks and Goodbye!!!");
     }
 }else if (currencyType == "EUR") {
-    alert(1 + "EUR" + " " + "=" + " " + 750 + " " + "Naira");
-    var currencyamount = prompt("How much do you want to convert?");
-    alert("Amount converted =" + " " + currencyamount * 750 + " " + "Naira");
+    alert("Conversion rate:" + " " + 1 + " " + "€" + " " + "=" + " " + 445.24240 + " " + "₦");
+    var currencyamount = prompt("How much [EUR] do you want to convert?");
+    alert("Amount converted =" + " " + currencyamount * 445.24240 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
     } else if (transaction_continues == "no"){
         alert("Thanks and Goodbye!!!");
     }
-}else if (currencyType == "Cedis") {
-    alert(1 + "Cedis" + " " + "=" + " " + 600 + " " + "Naira");
-    var currencyamount = prompt("How much do you want to convert?");
-    alert("Amount converted =" + " " + currencyamount * 600 + " " + "Naira");
+}else if (currencyType == "GHC") {
+    alert("Conversion rate:" + " " + 1 + " " + "GH¢" + " " + "=" + " " + 31.48190 + " " + "₦");
+    var currencyamount = prompt("How much [GHC] do you want to convert?");
+    alert("Amount converted =" + " " + currencyamount * 31.48190 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
     } else if (transaction_continues == "no"){
         alert("Thanks and Goodbye!!!");
     }
-}else if (currencyType == "Yen") {
-    alert(1 + "Yen" + " " + "=" + " " + 300 + " " + "Naira");
-    var currencyamount = prompt("How much do you want to convert?");
-    alert("Amount converted =" + " " + currencyamount * 300 + " " + "Naira");
+}else if (currencyType == "JPY") {
+    alert("Conversion rate:" + " " + 1 + " " + "¥" + " " + "=" + " " + 3.03320 + " " + "₦");
+    var currencyamount = prompt("How much [JPY] do you want to convert?");
+    alert("Amount converted =" + " " + currencyamount * 3.03320 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
     if(transaction_continues == "yes"){
         alert("Reload page and continue. Thanks!!!");
@@ -64,3 +64,11 @@ if(currencyType == "USD") {
 
 
 
+
+
+
+
+
+
+
+ 
