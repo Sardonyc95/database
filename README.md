0851894ded725ff4af71b4145af3313553cdc1f6
Amacito <amarachi5695@gmmail.com>
2022-11-14 16:29:28 +0100

    completed1

---
+++ README.md
@@ -0,0 +1,10 @@
+CURRENCY CONVERTER APP 
+
+naira_app.js:
+
+This appliation converts other currencies like USD, GBP, EUR,GHC, JPY, etc. to NGN
+
+
+allcurrency_app.js:
+
+This application helps in multiple conversion of one currency to another.
--- currency_converter/naira_app.js
+++ currency_converter/naira_app.js
@@ -10,57 +10,64 @@ alert("Hi, welcome to your naira currency converter");
 
 var currencyType = prompt("what currency do you want to convert to NGN? e.g USD, GBP, EUR, GHC, JPY, etc.");
 
-if(currencyType == "USD") {
+if (currencyType == "USD") {
     alert("Conversion rate:" + " " + 1 + " " + "$" + "=" + " " + 700 + " " + "₦");
     var currencyamount = prompt("How much [USD] do you want to convert?");
     alert("Amount converted =" + " " + currencyamount * 700 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
-    if(transaction_continues == "yes"){
+    if (transaction_continues == "yes") {
         alert("Reload page and continue. Thanks!!!");
-    } else if (transaction_continues == "no"){
+    } else if (transaction_continues == "no") {
         alert("Thanks and Goodbye!!!");
     }
-}else if (currencyType == "GBP") {
-    alert("Conversion rate:" + " " + 1 +" " +  "£" + " " + "=" + " " + 507.06370 + " " + "₦");
+    
+} else if (currencyType == "GBP") {
+    alert("Conversion rate:" + " " + 1 + " " + "£" + " " + "=" + " " + 507.06370 + " " + "₦");
     var currencyamount = prompt("How much [GBP] do you want to convert?");
     alert("Amount converted =" + " " + currencyamount * 507.06370 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
-    if(transaction_continues == "yes"){
+    if (transaction_continues == "yes") {
         alert("Reload page and continue. Thanks!!!");
-    } else if (transaction_continues == "no"){
+    } else if (transaction_continues == "no") {
         alert("Thanks and Goodbye!!!");
     }
-}else if (currencyType == "EUR") {
+
+} else if (currencyType == "EUR") {
     alert("Conversion rate:" + " " + 1 + " " + "€" + " " + "=" + " " + 445.24240 + " " + "₦");
     var currencyamount = prompt("How much [EUR] do you want to convert?");
     alert("Amount converted =" + " " + currencyamount * 445.24240 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
-    if(transaction_continues == "yes"){
+    if (transaction_continues == "yes") {
         alert("Reload page and continue. Thanks!!!");
-    } else if (transaction_continues == "no"){
+    } else if (transaction_continues == "no") {
         alert("Thanks and Goodbye!!!");
     }
-}else if (currencyType == "GHC") {
+    
+} else if (currencyType == "GHC") {
     alert("Conversion rate:" + " " + 1 + " " + "GH¢" + " " + "=" + " " + 31.48190 + " " + "₦");
     var currencyamount = prompt("How much [GHC] do you want to convert?");
     alert("Amount converted =" + " " + currencyamount * 31.48190 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
-    if(transaction_continues == "yes"){
+    if (transaction_continues == "yes") {
         alert("Reload page and continue. Thanks!!!");
-    } else if (transaction_continues == "no"){
+    } else if (transaction_continues == "no") {
         alert("Thanks and Goodbye!!!");
     }
-}else if (currencyType == "JPY") {
+    
+} else if (currencyType == "JPY") {
     alert("Conversion rate:" + " " + 1 + " " + "¥" + " " + "=" + " " + 3.03320 + " " + "₦");
     var currencyamount = prompt("How much [JPY] do you want to convert?");
     alert("Amount converted =" + " " + currencyamount * 3.03320 + " " + "₦");
     var transaction_continues = prompt("Do you want to perform another transaction?");
-    if(transaction_continues == "yes"){
+    if (transaction_continues == "yes") {
         alert("Reload page and continue. Thanks!!!");
-    } else if (transaction_continues == "no"){
+    } else if (transaction_continues == "no") {
         alert("Thanks and Goodbye!!!");
     }
+    
 }
+
+
 
 
 
